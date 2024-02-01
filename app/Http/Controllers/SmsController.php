0f2cadaf895f3e6f5920adc0b638750b2d962865
hasan mahmud <mh.mahmud.me@gmail.com>
2024-02-01 17:14:35 +0600

    additional commit

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -84,66 +84,11 @@ class SmsController extends Controller
 
     public function sms_template_destroy(Request $request)
     {
-        $data = [];
-        $request->validate([
-            'id' => 'required'
-        ]);
-
-        $check = SmsTemplate::find($request->id);
-        if(!empty($check)) {
-            $data['data'] = SmsTemplate::destroy($request->id);
-            $data['status'] = "success";
-            $data['msg'] = ["SmsTemplate deleted successfully"];
-            return response()->json(compact('data'))->setStatusCode(200);
-        }
-        $data['status'] = "failed";
-        $data['msg']= ["no data found"];
-        return response()->json(compact('data'))->setStatusCode(401);
+        return $this->service->sms_template_destroy_service($request);
     }
 
     public function send_sms(Request $request)
     {
-        $data = [];
-        $request->validate([
-            'user_id' => 'required',
-            'sms_from' => 'required',
-            'sms_to' => 'required',
-            'sms_text' => 'required'
-        ]);
-
-        if(empty($request->send_status)) {
-            $request['send_status'] = 0;
-        }
-
-        if(empty($request->priority_level)) {
-            $request['priority_level'] = 5;
-        }
-        $request['log_time'] = date("Y-m-d h:i:s", time());
-
-        //dd($request->all());
-
-        try {
-            /*$queue = new SmsQueue();
-            $SmsQueue->user_id = $request->user_id;
-            $SmsQueue->sms_from = $request->sms_from;
-            $SmsQueue->sms_to = $request->sms_to;
-            $SmsQueue->sms_text = $request->sms_text;
-            $SmsQueue->send_status = $request->send_status ?? 0;
-            $SmsQueue->campaign_id = $request->campaign_id ?? null;
-            $SmsQueue->priority_level = $request->priority_level ?? 5;
-            $SmsQueue->log_time = date("Y-m-d h:i:s", time());
-            $SmsQueue->save();*/
-
-            $data['data'] = SmsQueue::create($request->all());
-            $data['status'] = "success";
-            $data['msg']=["SmsTemplate saved successfully"];
-            return response()->json(compact('data'))->setStatusCode(200);
-
-        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
-            return response()->json(['error' => 'User not found'], 404);
-        } catch (\Exception $e) {
-            // Handle other types of exceptions
-            return response()->json(['error' => 'Something went wrong'], 500);
-        }
+        return $this->service->send_sms_service($request);
     }
 }
--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -1,10 +1,62 @@
 <?php
 namespace App\Services;
 use App\Models\SmsTemplates;
+use App\Models\SmsQueue;
+use App\Models\SmsLog;
 
 class SmsService {
 
 	public function get_all_templates() {
 		return SmsTemplates::all();
+	}
+
+	public function sms_template_destroy_service() {
+        $data = [];
+        $request->validate([
+            'id' => 'required'
+        ]);
+
+        $check = SmsTemplate::find($request->id);
+        if(!empty($check)) {
+            $data['data'] = SmsTemplate::destroy($request->id);
+            $data['status'] = "success";
+            $data['msg'] = ["SmsTemplate deleted successfully"];
+            return response()->json(compact('data'))->setStatusCode(200);
+        }
+        $data['status'] = "failed";
+        $data['msg']= ["no data found"];
+        return response()->json(compact('data'))->setStatusCode(401);
+	}
+
+	public function send_sms_service($request) {
+        $data = [];
+        $request->validate([
+            'user_id' => 'required',
+            'sms_from' => 'required',
+            'sms_to' => 'required',
+            'sms_text' => 'required'
+        ]);
+
+        if(empty($request->send_status)) {
+            $request['send_status'] = 0;
+        }
+
+        if(empty($request->priority_level)) {
+            $request['priority_level'] = 5;
+        }
+        $request['log_time'] = date("Y-m-d h:i:s", time());
+
+        try {
+            $data['data'] = SmsQueue::create($request->all());
+            $data['status'] = "success";
+            $data['msg']=["Sms sending is on process successfully"];
+            return response()->json(compact('data'))->setStatusCode(200);
+
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json(['error' => 'User not found'], 404);
+        } catch (\Exception $e) {
+            // Handle other types of exceptions
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
 	}
 }
