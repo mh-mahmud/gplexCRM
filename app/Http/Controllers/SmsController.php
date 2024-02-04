3c97b72cf1245f4a6375933a0f84b5ab677ce43e
hasan mahmud <mh.mahmud.me@gmail.com>
2024-02-04 15:55:01 +0600

    sms service update

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -21,65 +21,22 @@ class SmsController extends Controller
         
     public function sms_template_list()
     {
-        return SmsTemplate::all();
+        return $this->service->get_all_templates();
     }
 
     public function sms_template_create(Request $request)
     {
-        $data = [];
-        $request->validate([
-            'title' => 'required',
-            'description' => 'required',
-            'status' => 'required'
-        ]);
-
-        $ckh_dub = SmsTemplate::where('title', $request->title)->first();
-        if(!empty($ckh_dub)) {
-            $data['status'] = "dublicate";
-            $data['msg']=["this template already created"];
-            return response()->json(compact('data'))->setStatusCode(200);
-        }
-
-        $data['data'] = SmsTemplate::create($request->all());
-        $data['status'] = "success";
-        $data['msg']=["SmsTemplate saved successfully"];
-        return response()->json(compact('data'))->setStatusCode(200);
+        return $this->service->sms_template_create_service($request);
     }
 
     public function sms_template_show(string $id)
     {
-        $data = [];
-        $chk_data = SmsTemplate::find($id);
-        if(!empty($chk_data)) {
-            $status = "success";
-            $msg=["data found"];
-            $data['data'] = $chk_data;
-            return response()->json(compact('data'))->setStatusCode(200);
-        }
-        $data['status'] = "failed";
-        $data['msg']=["no data found"];
-        return response()->json(compact('data'))->setStatusCode(401);
+        return $this->service->sms_template_show_service($id);
     }
 
     public function sms_template_update(Request $request)
     {
-        $data = [];
-        $request->validate([
-            'id' => 'required'
-        ]);
-
-        $branch_data = SmsTemplate::find($request->id);
-        if(!empty($branch_data)) {
-            $branch_data->update($request->all());
-            $data['status'] = "success";
-            $data['msg']=["SmsTemplate updated successfully"];
-            $data['data'] = $branch_data;
-            return response()->json(compact('data'))->setStatusCode(200);
-        }
-
-        $data['status'] = "failed";
-        $data['msg']=["no data found"];
-        return response()->json(compact('data'))->setStatusCode(401);
+        return $this->service->sms_template_update_service($request);
     }
 
     public function sms_template_destroy(Request $request)
--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -1,13 +1,13 @@
 <?php
 namespace App\Services;
-use App\Models\SmsTemplates;
+use App\Models\SmsTemplate;
 use App\Models\SmsQueue;
 use App\Models\SmsLog;
 
 class SmsService {
 
 	public function get_all_templates() {
-		return SmsTemplates::all();
+		return SmsTemplate::paginate(2);
 	}
 
 	public function sms_template_destroy_service() {
@@ -58,5 +58,60 @@ class SmsService {
             // Handle other types of exceptions
             return response()->json(['error' => $e->getMessage()], 500);
         }
+	}
+
+	public function sms_template_update_service() {
+        $data = [];
+        $request->validate([
+            'id' => 'required'
+        ]);
+
+        $branch_data = SmsTemplate::find($request->id);
+        if(!empty($branch_data)) {
+            $branch_data->update($request->all());
+            $data['status'] = "success";
+            $data['msg']=["SmsTemplate updated successfully"];
+            $data['data'] = $branch_data;
+            return response()->json(compact('data'))->setStatusCode(200);
+        }
+
+        $data['status'] = "failed";
+        $data['msg']=["no data found"];
+        return response()->json(compact('data'))->setStatusCode(401);
+	}
+
+	public function sms_template_show_service($id) {
+        $data = [];
+        $chk_data = SmsTemplate::find($id);
+        if(!empty($chk_data)) {
+            $status = "success";
+            $msg=["data found"];
+            $data['data'] = $chk_data;
+            return response()->json(compact('data'))->setStatusCode(200);
+        }
+        $data['status'] = "failed";
+        $data['msg']=["no data found"];
+        return response()->json(compact('data'))->setStatusCode(401);
+	}
+
+	public function sms_template_create_service($request) {
+        $data = [];
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'status' => 'required'
+        ]);
+
+        $ckh_dub = SmsTemplate::where('title', $request->title)->first();
+        if(!empty($ckh_dub)) {
+            $data['status'] = "dublicate";
+            $data['msg']=["this template already created"];
+            return response()->json(compact('data'))->setStatusCode(200);
+        }
+
+        $data['data'] = SmsTemplate::create($request->all());
+        $data['status'] = "success";
+        $data['msg']=["SmsTemplate saved successfully"];
+        return response()->json(compact('data'))->setStatusCode(200);
 	}
 }
