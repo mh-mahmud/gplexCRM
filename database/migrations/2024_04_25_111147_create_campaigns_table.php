ce6e0ac5ee105a454a3e7090d999d27ca648bfd6
Rokibuzzaman <sourovh207@gmail.com>
2024-04-25 17:15:44 +0600

    merge master promotion,campaign table

---
+++ database/migrations/2024_04_25_111147_create_campaigns_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->string('campaign_title');
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->text('description')->nullable();
+            $table->string('campaign_type')->nullable();
+            $table->integer('campaign_limit')->nullable();
+            $table->string('campaign_service')->nullable();
+            $table->tinyInteger('status');
+            $table->unsignedBigInteger('promotion_id')->nullable();
+            $table->foreign('promotion_id')->references('id')->on('promotions')->onDelete('cascade');
+            $table->timestamps();
+        });
+        Schema::table('campaigns', function (Blueprint $table) {
+            $table->index('promotion_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaigns');
+    }
+};
