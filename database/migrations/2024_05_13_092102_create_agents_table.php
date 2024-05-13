8e528159c66c2009ffc08c5a948484203ac92ff2
Rokibuzzaman <sourovh207@gmail.com>
2024-05-13 15:50:22 +0600

    agent table migration

---
+++ database/migrations/2024_05_13_092102_create_agents_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agents', function (Blueprint $table) {
+            $table->char('agent_id', 4)->primary();
+            $table->string('name', 191);
+            $table->string('phone_number', 191)->nullable();
+            $table->date('birth_day')->nullable();
+            $table->string('profile_image', 191)->nullable();
+            $table->tinyInteger('status')->default(0); // Change default status if necessary
+            $table->char('role_id', 10)->nullable();
+            $table->char('did', 10)->nullable();
+            $table->char('seat_id', 3)->nullable();
+            $table->char('skill_id', 2)->nullable();
+            $table->string('gender', 191)->nullable();
+            $table->text('address')->nullable();
+            $table->text('description')->nullable();
+            $table->string('performance', 191)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agents');
+    }
+};
