f1161a75c2a10b2c0cb3d79ebdb7c0dbdaea5c4b
Rokibuzzaman <sourovh207@gmail.com>
2024-05-14 13:42:50 +0600

    agent crud controller

--- resources/views/agents/create.blade.php
+++ resources/views/agents/create.blade.php
@@ -302,13 +302,12 @@
 
                                         </div>
                                         <!--End Row-->
-                                <div class="card-footer d-flex justify-content-end py-6 px-9">
-                                    <button type="reset" class="btn btn-light btn-active-light-primary me-2">Discard
-                                    </button>
-                                    <button type="submit" class="btn btn-primary"
-                                            id="kt_account_profile_details_submit">Save Changes
-                                    </button>
-                                </div>
+                                      <div class="card-footer d-flex justify-content-end py-6 px-9">
+                                        <a href="{{ route('agents.create') }}" class="btn btn-light btn-active-light-primary me-2">Reset</a>
+                                            <button type="submit" class="btn btn-primary"
+                                                    id="kt_account_profile_details_submit">Save Changes
+                                            </button>
+                                        </div>
 
                                     </form>
 
