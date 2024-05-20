d0b496dea6a618fd5f337c58d5f5d76803c3f875
Rokibuzzaman <sourovh207@gmail.com>
2024-05-20 18:47:55 +0600

    lead form crud with design

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1709,7 +1709,7 @@
 										<!--begin::Menu-->
 										<div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true">
 											<div class="menu-item me-lg-1">
-												<a class="menu-link active py-3" href="../dist/index.html">
+												<a class="menu-link active py-3" href="{{ route('dashboard') }}">
 													<span class="menu-title">Dashboard</span>
 												</a>
 											</div>
