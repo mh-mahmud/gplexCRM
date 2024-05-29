a22697d4ea1bd62b38562c4bc9bbecd6e61f77b3
Rokibuzzaman <sourovh207@gmail.com>
2024-05-29 18:08:39 +0600

    lead dynamic table create update delete view

--- resources/views/dynamic_table/index.blade.php
+++ resources/views/dynamic_table/index.blade.php
@@ -258,7 +258,7 @@
 											</span>
 									<!--end::Svg Icon-->
 								</a>
-								<a href=""
+								<a href="{{ route('dynamic_table.edit', $dynamicTable->table_name) }}"
 								   class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
 									<!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
 									<span class="svg-icon svg-icon-3">
