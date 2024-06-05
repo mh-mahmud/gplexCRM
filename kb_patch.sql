1136406b2b6349766d6781ffc99e0924ebf56353
hasan mahmud <mh.mahmud.me@gmail.com>
2024-06-05 18:10:44 +0600

    edit role option done

--- kb_patch.sql
+++ kb_patch.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `permissions` CHANGE `permission_group_id` `permission_group_id` BIGINT(20) UNSIGNED NULL DEFAULT NULL;
 ALTER TABLE `permissions` CHANGE `id` `id` INT NOT NULL AUTO_INCREMENT;
 ALTER TABLE `roles` CHANGE `details` `permision_details` JSON NULL DEFAULT NULL;
+ALTER TABLE `roles` ADD `permission_ids` JSON NULL DEFAULT NULL AFTER `permission_details`;
