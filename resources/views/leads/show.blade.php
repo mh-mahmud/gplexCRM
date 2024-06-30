afbd76d4cef1ed6206157668379c9ffb69083e59
Rokibuzzaman <sourovh207@gmail.com>
2024-06-30 17:47:16 +0600

    update lead form

--- resources/views/leads/show.blade.php
+++ resources/views/leads/show.blade.php
@@ -187,7 +187,7 @@ use Carbon\Carbon;
                         </span>
                     </div>
                     <div class="d-flex align-items-center gap-2 bg-light p-3 mb-1">
-                        <span class="fs-6 fw-bolder mb-1 text-gray-900 text-hover-primary w-lg-100px w-xxl-150px">Alternative Number</span>
+                        <span class="fs-6 fw-bolder mb-1 text-gray-900 text-hover-primary w-lg-100px w-xxl-150px">Age</span>
                         <span>{{ $lead->age }}</span>
                     </div>
                     <div class="d-flex align-items-center gap-2 bg-light p-3 mb-1">
