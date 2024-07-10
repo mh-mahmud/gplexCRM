4ab4fd7c7ea806acc49657611e0ca24f9771aef4
Momin Riyadh <mominriyadh@gmail.com>
2024-07-10 10:49:00 +0600

    fix and deploy table alignment

--- resources/views/sms/template-list.blade.php
+++ resources/views/sms/template-list.blade.php
@@ -144,11 +144,15 @@
                                                     <span class="badge badge-light-danger">Inactive</span>
                                                 @endif
                                             </td>
-                                            <td class="text-end">
-                                                <a href="{{ route('sms-template-show', $template->id) }}"
-                                                   class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
-                                                    <!--begin::Svg Icon | path: icons/duotune/general/gen019.svg-->
-                                                    <span class="svg-icon svg-icon-3">
+
+                                            {{--Fixme:Vertical Alignment--}}
+                                            <td>
+                                                <div
+                                                    class="d-inline-flex justify-content-end gap-1 w-100 border-bottom-0">
+                                                    <a href="{{ route('sms-template-show', $template->id) }}"
+                                                       class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
+                                                        <!--begin::Svg Icon | path: icons/duotune/general/gen019.svg-->
+                                                        <span class="svg-icon svg-icon-3">
 												<svg xmlns="http://www.w3.org/2000/svg"
                                                      width="24px" height="24px" viewBox="0 0 24 24">
 														<g stroke="none" stroke-width="1"
@@ -165,12 +169,12 @@
 														</g>
 													</svg>
 											</span>
-                                                    <!--end::Svg Icon-->
-                                                </a>
-                                                <a href="{{ route('sms-template-edit', $template->id) }}"
-                                                   class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
-                                                    <!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
-                                                    <span class="svg-icon svg-icon-3">
+                                                        <!--end::Svg Icon-->
+                                                    </a>
+                                                    <a href="{{ route('sms-template-edit', $template->id) }}"
+                                                       class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
+                                                        <!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
+                                                        <span class="svg-icon svg-icon-3">
 												<svg xmlns="http://www.w3.org/2000/svg" width="24"
                                                      height="24" viewBox="0 0 24 24" fill="none">
 													<path opacity="0.3"
@@ -181,17 +185,17 @@
                                                         fill="black"/>
 												</svg>
 											</span>
-                                                    <!--end::Svg Icon-->
-                                                </a>
-                                                <form action="{{ route('sms-template-delete', $template->id) }}"
-                                                      method="POST" style="display: inline;">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit"
-                                                            class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm"
-                                                            onclick="return confirmDelete()">
-                                                        <!--begin::Svg Icon | path: icons/duotune/general/gen027.svg-->
-                                                        <span class="svg-icon svg-icon-3">
+                                                        <!--end::Svg Icon-->
+                                                    </a>
+                                                    <form action="{{ route('sms-template-delete', $template->id) }}"
+                                                          method="POST" style="display: inline;">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit"
+                                                                class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm"
+                                                                onclick="return confirmDelete()">
+                                                            <!--begin::Svg Icon | path: icons/duotune/general/gen027.svg-->
+                                                            <span class="svg-icon svg-icon-3">
 											<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                  viewBox="0 0 24 24" fill="none">
 												<path
@@ -205,9 +209,10 @@
                                                       fill="black"/>
 											</svg>
 										</span>
-                                                        <!--end::Svg Icon-->
-                                                    </button>
-                                                </form>
+                                                            <!--end::Svg Icon-->
+                                                        </button>
+                                                    </form>
+                                                </div>
                                             </td>
                                         </tr>
                                     @endforeach
