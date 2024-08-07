4c4bd80cd742f3a4ee6167b4ccb54ac5c6468861
Ishtiak Ahmed <ishtiak.ia@gmail.com>
2024-08-07 14:26:52 +0600

    proposal

--- kb_patch.sql
+++ kb_patch.sql
@@ -52,6 +52,69 @@ ALTER TABLE `logs`
 ALTER TABLE `logs`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+CREATE TABLE `countries` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `countries`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+
+CREATE TABLE `currencies` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `symbol` varchar(10) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `currencies`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `currencies`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+CREATE TABLE `proposals` (
+  `id` bigint(20) NOT NULL,
+  `subject` varchar(250) NOT NULL,
+  `lead_id` bigint(20) DEFAULT NULL,
+  `customer_id` bigint(20) DEFAULT NULL,
+  `start_date` timestamp NULL DEFAULT NULL,
+  `end_date` timestamp NULL DEFAULT NULL,
+  `currency` tinyint(4) DEFAULT NULL,
+  `assigned_agent_id` bigint(20) NOT NULL,
+  `send_to` varchar(250) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `city` varchar(250) DEFAULT NULL,
+  `state` varchar(250) DEFAULT NULL,
+  `country_id` bigint(20) DEFAULT NULL,
+  `zip_code` varchar(10) DEFAULT NULL,
+  `send_to_email` varchar(250) DEFAULT NULL,
+  `send_to_phone` varchar(20) DEFAULT NULL,
+  `discount` decimal(10,0) DEFAULT NULL,
+  `adjustment` decimal(10,0) DEFAULT NULL,
+  `sub_total` decimal(10,0) DEFAULT NULL,
+  `total` decimal(10,0) DEFAULT NULL,
+  `status` tinyint(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `proposals`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `proposals`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
 ---Ishtiak SQL end
 
 CREATE TABLE customers LIKE leads;
