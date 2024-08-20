57bf2f328205050095f24c4f478e5a9288bd0d7c
Rokibuzzaman <sourovh207@gmail.com>
2024-08-20 10:21:10 +0600

    lead panel file upload option

--- resources/views/leads/edit_table_data.blade.php
+++ resources/views/leads/edit_table_data.blade.php
@@ -98,7 +98,9 @@
                                     @elseif($inputType === 'dropdown')
                                     <select class="form-control form-control-sm form-control-solid" id="{{ $column }}" name="{{ $column }}">
                                         @foreach($dropdownOptions[$column] as $option)
-                                        <option value="{{ $option }}">{{ ucfirst($option) }}</option>
+                                        <option value="{{ $option }}" {{ $existingData->$column == $option ? 'selected' : '' }}>
+                                            {{ ucfirst($option) }}
+                                        </option>
                                         @endforeach
                                     </select>
                                     @else
