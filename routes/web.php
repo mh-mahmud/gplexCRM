eb0d1b8576127dafd28395b883a10a9ec7d487e5
Hasan <mh.mahmud.me@gmail.com>
2024-09-26 10:22:47 +0600

    route file updated

--- routes/web.php
+++ routes/web.php
@@ -35,15 +35,7 @@ use App\Models\Promotion;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-//Route::get('/login', [AuthController::class, 'index'])->name('login');
-//Route::post('/post_login', [AuthController::class, 'postLogin'])->name('login.post');
-/*Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
-Route::get('/profile', [DashboardController::class, 'profile'])->name('profile');*/
 Route::get('/', [AuthController::class, 'index'])->name('login_index');
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/post_login', [AuthController::class, 'postLogin'])->name('login.post');
