5772e70410244a2e2f591d03875f0490810b4d9b
Hasan <mh.mahmud.me@gmail.com>
2024-10-14 08:52:56 +0600

    common folder add for static images

--- resources/views/proposals/show.blade.php
+++ resources/views/proposals/show.blade.php
@@ -28,103 +28,7 @@
             <!--end::Page title-->
             <!--begin::Actions-->
             <div class="d-flex align-items-center py-1">
-                <!--begin::Wrapper-->
-                <div class="me-4">
-                    <!--begin::Menu-->
 
-                    <!--begin::Menu 1-->
-                    <div class="menu menu-sub menu-sub-dropdown w-250px w-md-300px" data-kt-menu="true"
-                         id="kt_menu_61484bf44d957">
-                        <!--begin::Header-->
-                        <div class="px-7 py-5">
-                            <div class="fs-5 text-dark fw-bolder">Filter Options</div>
-                        </div>
-                        <!--end::Header-->
-                        <!--begin::Menu separator-->
-                        <div class="separator border-gray-200"></div>
-                        <!--end::Menu separator-->
-                        <!--begin::Form-->
-                        <div class="px-7 py-5">
-                            <!--begin::Input group-->
-                            <div class="mb-10">
-                                <!--begin::Label-->
-                                <label class="form-label fw-bold">Status:</label>
-                                <!--end::Label-->
-                                <!--begin::Input-->
-                                <div>
-                                    <select class="form-select form-select-solid" data-kt-select2="true"
-                                            data-placeholder="Select option"
-                                            data-dropdown-parent="#kt_menu_61484bf44d957"
-                                            data-allow-clear="true">
-                                        <option></option>
-                                        <option value="1">Approved</option>
-                                        <option value="2">Pending</option>
-                                        <option value="2">In Process</option>
-                                        <option value="2">Rejected</option>
-                                    </select>
-                                </div>
-                                <!--end::Input-->
-                            </div>
-                            <!--end::Input group-->
-                            <!--begin::Input group-->
-                            <div class="mb-10">
-                                <!--begin::Label-->
-                                <label class="form-label fw-bold">Member Type:</label>
-                                <!--end::Label-->
-                                <!--begin::Options-->
-                                <div class="d-flex">
-                                    <!--begin::Options-->
-                                    <label
-                                        class="form-check form-check-sm form-check-custom form-check-solid me-5">
-                                        <input class="form-check-input" type="checkbox" value="1"/>
-                                        <span class="form-check-label">Author</span>
-                                    </label>
-                                    <!--end::Options-->
-                                    <!--begin::Options-->
-                                    <label
-                                        class="form-check form-check-sm form-check-custom form-check-solid">
-                                        <input class="form-check-input" type="checkbox" value="2"
-                                               checked="checked"/>
-                                        <span class="form-check-label">Customer</span>
-                                    </label>
-                                    <!--end::Options-->
-                                </div>
-                                <!--end::Options-->
-                            </div>
-                            <!--end::Input group-->
-                            <!--begin::Input group-->
-                            <div class="mb-10">
-                                <!--begin::Label-->
-                                <label class="form-label fw-bold">Notifications:</label>
-                                <!--end::Label-->
-                                <!--begin::Switch-->
-                                <div
-                                    class="form-check form-switch form-switch-sm form-check-custom form-check-solid">
-                                    <input class="form-check-input" type="checkbox" value=""
-                                           name="notifications" checked="checked"/>
-                                    <label class="form-check-label">Enabled</label>
-                                </div>
-                                <!--end::Switch-->
-                            </div>
-                            <!--end::Input group-->
-                            <!--begin::Actions-->
-                            <div class="d-flex justify-content-end">
-                                <button type="reset"
-                                        class="btn btn-sm btn-light btn-active-light-primary me-2"
-                                        data-kt-menu-dismiss="true">Reset
-                                </button>
-                                <button type="submit" class="btn btn-sm btn-primary"
-                                        data-kt-menu-dismiss="true">Apply
-                                </button>
-                            </div>
-                            <!--end::Actions-->
-                        </div>
-                        <!--end::Form-->
-                    </div>
-                    <!--end::Menu 1-->
-                    <!--end::Menu-->
-                </div>
-                <!--end::Wrapper-->
                 <!--begin::Button-->
                 <a href="{{ route('proposal-list') }}" class="btn btn-sm btn-primary" id="kt_toolbar_primary_button">Proposal List</a>
                 <!--end::Button-->
@@ -156,12 +60,12 @@
                             <div class="me-7 mb-4">
                                 <div class="symbol symbol-100px symbol-lg-140px symbol-fixed position-relative">
                                     @if($data->proposal_file_name != '')
-                                        <a href="{{ url('uploads/proposals/'.$data->proposal_file_name) }}" download="{{$data->proposal_file_name}}"><img src="{{ asset('uploads/proposals/file-test.png') }}" alt="{{ $data->proposal_file_name }}"></a>
+                                        <a href="{{ url('uploads/proposals/'.$data->proposal_file_name) }}" download="{{$data->proposal_file_name}}"><img src="{{ asset('uploads/common/file-test.png') }}" alt="{{ $data->proposal_file_name }}"></a>
                                     @else
-                                        <img alt="Logo" src="{{ asset('uploads/noimage.jpg') }}"/>
+                                        <img alt="Logo" src="{{ asset('uploads/common/no-file.png') }}"/>
                                     @endif
 
-<!-- <a href="{{ url('files/sample.xlsx') }}" download="sample.xlsx">Download Sample File</a> -->
+
 
                                 </div>
                             </div>
