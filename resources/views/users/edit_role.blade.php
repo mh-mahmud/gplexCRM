9f7b0e0c0699210261d4c95797c6c935ccddeea0
Hasan <mh.mahmud.me@gmail.com>
2024-10-29 17:42:54 +0600

    route data fix

--- resources/views/users/edit_role.blade.php
+++ resources/views/users/edit_role.blade.php
@@ -189,9 +189,7 @@
                                         </div>
                                         <!--End Row-->
                                       <div class="card-footer d-flex justify-content-end py-6 px-9">
-                                        <a href="{{ route('create-user') }}" class="btn btn-light btn-active-light-primary me-2">Reset</a>
-                                            <button type="submit" class="btn btn-primary"
-                                                    id="kt_account_profile_details_submit">Submit
+                                            <button type="submit" class="btn btn-primary" id="kt_account_profile_details_submit">Submit
                                             </button>
                                         </div>
 
