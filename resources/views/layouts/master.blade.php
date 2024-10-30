6dd71d25870eab7d61650010b40ffe004c224c74
momin-riyadh <mominriyadh@gmail.com>
2024-10-30 11:05:34 +0600

    fix and deploy

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -48,8 +48,7 @@
 				<div class="aside-logo flex-column-auto" id="kt_aside_logo">
 					<!--begin::Logo-->
 					<a href="/">
-{{--						<img alt="Logo" src="{{url('/')}}/assets/media/logos/logo-1-dark.svg" class="h-50px logo" />--}}
-						<img width="150" alt="Logo" src="{{url('/')}}/assets/media/logos/logo-1-dark.png" class="logo" />
+						<img alt="Logo" src="{{url('/')}}/assets/media/logos/logo-1-dark.svg" class="h-50px logo" />
 					</a>
 					<!--end::Logo-->
 					<!--begin::Aside toggler-->
@@ -716,7 +715,7 @@
 						<!--begin::Copyright-->
 						<div class="text-dark order-2 order-md-1">
 							<span class="text-muted fw-bold me-1">2024©</span>
-							<a href="https://www.genuitysystems.com/" target="_blank" class="text-gray-800 text-hover-primary">Genuity Sytems Ltd</a>
+							<a href="https://www.gplex.com/" target="_blank" class="text-gray-800 text-hover-primary">Powered by <span class="text-danger">g</span>Plex</a>
 						</div>
 						<!--end::Copyright-->
 						<!--begin::Menu-->
