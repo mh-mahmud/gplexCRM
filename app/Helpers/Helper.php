59b684ead5e4d7fe37325468bdcd841d9f80ca2c
Hasan <mh.mahmud.me@gmail.com>
2024-11-03 17:57:57 +0600

    customer panel update

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -21,14 +21,15 @@ class Helper
 
     }
 
-    public static function storeLog($log_text, $module_name, $sub_module_name, $action, $lead_id=null)
+    public static function storeLog($log_text, $module_name, $sub_module_name, $action=null, $lead_id=null)
     {
         $log                    = new Logs();
         $log->user_id           = Auth::id();
         $log->lead_id           = $lead_id;
         $log->module            = $module_name;
         $log->sub_module        = $sub_module_name;
-        $log->log_message       = $log_text." => ".$module_name." => ".$action;
+        // $log->log_message       = $log_text." => ".$module_name." => ".$action;
+        $log->log_message       = $log_text;
         $log->status            = 1;
         $log->save();
     }
