0969744b608314305dd0e0038e9de867bfcd1862
Rokibuzzaman <sourovh207@gmail.com>
2024-12-29 17:09:05 +0600

    invoice edit

--- resources/views/leads/show.blade.php
+++ resources/views/leads/show.blade.php
@@ -328,6 +328,12 @@
 
                                     <div class="d-flex align-items-center gap-2 bg-light p-1 mb-1">
                                     <span
+                                        class="fs-7 fw-bolder mb-1 text-gray-900 text-hover-primary w-lg-100px w-xxl-150px flex-shrink-0">Lead Start Date</span>
+                                        <span>{{ $lead->lead_start_date ? \Carbon\Carbon::parse($lead->lead_start_date)->format('d-m-Y') : '' }}</span>
+                                    </div>
+
+                                    <div class="d-flex align-items-center gap-2 bg-light p-1 mb-1">
+                                    <span
                                         class="fs-7 fw-bolder mb-1 text-gray-900 text-hover-primary w-lg-100px w-xxl-150px flex-shrink-0">Lead Notes</span>
                                         <span>{{ $lead->lead_notes }}</span>
                                     </div>
