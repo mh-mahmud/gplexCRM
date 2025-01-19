f98b9303c586b163ac44ee99f3fb0ed653519531
Ishtiak <ishtiak@genusys.us>
2025-01-19 10:16:19 +0600

    task

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -15,7 +15,7 @@ class TaskService
         if(Auth::user()->user_type != 'admin') {
             $sql->where('assigned_to', Auth::id());
         } else {
-            $sql->join('users', 'users.id', '=', 'tasks.assigned_to')
+            $sql->leftJoin('users', 'users.id', '=', 'tasks.assigned_to')
                 ->select('tasks.*', 'users.first_name', 'users.last_name');
         }
         return $sql->orderBy('id', 'DESC')->paginate(config('constants.ROW_PER_PAGE'));
