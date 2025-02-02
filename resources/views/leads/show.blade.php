ee8a774456b63370f29cf5a50e2bdd7f848f8428
Hasan <mh.mahmud.me@gmail.com>
2025-02-02 11:02:17 +0600

    update design issue

--- resources/views/leads/show.blade.php
+++ resources/views/leads/show.blade.php
@@ -1833,7 +1833,7 @@
             <!--begin::Body-->
             <div class="card-body position-relative" id="kt_activities_body">
                 <!--begin::Content-->
-                <div id="kt_activities_scroll" class="position-relative scroll-y me-n5 pe-5" data-kt-scroll="true" data-kt-scroll-height="auto" data-kt-scroll-wrappers="#kt_activities_body" data-kt-scroll-dependencies="#kt_activities_header, #kt_activities_footer" data-kt-scroll-offset="5px">
+                <div id="kt_activities_scroll" class="position-relative scroll-y me-n5 pe-5" data-kt-scroll="false" data-kt-scroll-height="auto" data-kt-scroll-wrappers="#kt_activities_body" data-kt-scroll-dependencies="#kt_activities_header, #kt_activities_footer" data-kt-scroll-offset="5px">
                     <!--begin::Timeline items-->
                     <!-- <div class="timeline"> -->
 
@@ -1931,7 +1931,7 @@
             <!--begin::Body-->
             <div class="card-body position-relative" id="kt_activities_body">
                 <!--begin::Content-->
-                <div id="kt_activities_scroll" class="position-relative scroll-y me-n5 pe-5" data-kt-scroll="true" data-kt-scroll-height="auto" data-kt-scroll-wrappers="#kt_activities_body" data-kt-scroll-dependencies="#kt_activities_header, #kt_activities_footer" data-kt-scroll-offset="5px">
+                <div id="kt_activities_scroll" class="position-relative scroll-y me-n5 pe-5" data-kt-scroll="false" data-kt-scroll-height="auto" data-kt-scroll-wrappers="#kt_activities_body" data-kt-scroll-dependencies="#kt_activities_header, #kt_activities_footer" data-kt-scroll-offset="5px">
                     <!--begin::Timeline items-->
                     <!-- <div class="timeline"> -->
 
