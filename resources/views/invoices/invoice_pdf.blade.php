da0662ad4f7aa832bd9872c46d2931d665640ed6
Rokibuzzaman <sourovh207@gmail.com>
2025-03-03 11:45:44 +0600

    quick lead add

--- resources/views/invoices/invoice_pdf.blade.php
+++ resources/views/invoices/invoice_pdf.blade.php
@@ -221,10 +221,10 @@
     <table style="border-collapse: collapse; border: none;" role="presentation" aria-label="Invoice Header">
         <tr>
             <td style="border: none; padding-bottom: 10px; ">
-                <img src="{{ $logogenuity }}" width="200" alt="Logo">
+            <img src="http://192.168.11.123/gplexCRM/uploads/invoice/genuity.png" width="200" alt="Logo">
             </td>
             <td style="border: none; padding-bottom: 10px; text-align: right">
-                <img src="{{ $logogplex }}" width="200" alt="Logo">
+            <img src="http://192.168.11.123/gplexCRM/uploads/invoice/gplex.png" width="200" alt="Logo">
             </td>
         </tr>
     </table>
