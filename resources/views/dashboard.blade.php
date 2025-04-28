bfaa8647e4a1ff5c05cf69628086cc0b47aed271
Rokibuzzaman <sourovh207@gmail.com>
2025-04-28 09:40:00 +0600

    dashboard chart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -382,6 +382,184 @@
                 </div>
                 <!--end::Row-->
 
+                <div class="row g-5 g-xl-8">
+									<div class="col-xl-6">
+										<!--begin::Charts Widget 3-->
+										<div class="card card-xl-stretch mb-xl-8">
+											<!--begin::Header-->
+											<div class="card-header border-0 pt-5">
+												<h3 class="card-title align-items-start flex-column">
+													<span class="card-label fw-bolder fs-3 mb-1">Recent Transactions</span>
+													<span class="text-muted fw-bold fs-7">More than 1000 new records</span>
+												</h3>
+												<!--begin::Toolbar-->
+												<div class="card-toolbar" data-kt-buttons="true">
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary active px-4 me-1" id="kt_charts_widget_3_year_btn">Year</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4 me-1" id="kt_charts_widget_3_month_btn">Month</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4" id="kt_charts_widget_3_week_btn">Week</a>
+												</div>
+												<!--end::Toolbar-->
+											</div>
+											<!--end::Header-->
+											<!--begin::Body-->
+											<div class="card-body">
+												<!--begin::Chart-->
+												<div id="kt_charts_widget_3_chart" style="height: 350px"></div>
+												<!--end::Chart-->
+											</div>
+											<!--end::Body-->
+										</div>
+										<!--end::Charts Widget 3-->
+									</div>
+									<div class="col-xl-6">
+										<!--begin::Charts Widget 4-->
+										<div class="card card-xl-stretch mb-5 mb-xl-8">
+											<!--begin::Header-->
+											<div class="card-header border-0 pt-5">
+												<h3 class="card-title align-items-start flex-column">
+													<span class="card-label fw-bolder fs-3 mb-1">Recent Customers</span>
+													<span class="text-muted fw-bold fs-7">More than 500 new customers</span>
+												</h3>
+												<!--begin::Toolbar-->
+												<div class="card-toolbar" data-kt-buttons="true">
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary active px-4 me-1" id="kt_charts_widget_4_year_btn">Year</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4 me-1" id="kt_charts_widget_4_month_btn">Month</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4" id="kt_charts_widget_4_week_btn">Week</a>
+												</div>
+												<!--end::Toolbar-->
+											</div>
+											<!--end::Header-->
+											<!--begin::Body-->
+											<div class="card-body">
+												<!--begin::Chart-->
+												<div id="kt_charts_widget_4_chart" style="height: 350px"></div>
+												<!--end::Chart-->
+											</div>
+											<!--end::Body-->
+										</div>
+										<!--end::Charts Widget 4-->
+									</div>
+								</div>
+								<!--end::Row-->
+								<!--begin::Row-->
+								<div class="row g-5 g-xl-8">
+									<div class="col-xl-6">
+										<!--begin::Charts Widget 5-->
+										<div class="card card-xl-stretch mb-xl-8">
+											<!--begin::Header-->
+											<div class="card-header border-0 pt-5">
+												<h3 class="card-title align-items-start flex-column">
+													<span class="card-label fw-bolder fs-3 mb-1">Recent Customers</span>
+													<span class="text-muted fw-bold fs-7">More than 500 new customers</span>
+												</h3>
+												<!--begin::Toolbar-->
+												<div class="card-toolbar" data-kt-buttons="true">
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary active px-4 me-1" id="kt_charts_widget_5_year_btn">Year</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4 me-1" id="kt_charts_widget_5_month_btn">Month</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4" id="kt_charts_widget_5_week_btn">Week</a>
+												</div>
+												<!--end::Toolbar-->
+											</div>
+											<!--end::Header-->
+											<!--begin::Body-->
+											<div class="card-body">
+												<!--begin::Chart-->
+												<div id="kt_charts_widget_5_chart" style="height: 350px"></div>
+												<!--end::Chart-->
+											</div>
+											<!--end::Body-->
+										</div>
+										<!--end::Charts Widget 5-->
+									</div>
+									<div class="col-xl-6">
+										<!--begin::Charts Widget 5-->
+										<div class="card card-xl-stretch mb-5 mb-xl-8">
+											<!--begin::Header-->
+											<div class="card-header border-0 pt-5">
+												<h3 class="card-title align-items-start flex-column">
+													<span class="card-label fw-bolder fs-3 mb-1">Recent Orders</span>
+													<span class="text-muted fw-bold fs-7">More than 500+ new orders</span>
+												</h3>
+												<!--begin::Toolbar-->
+												<div class="card-toolbar" data-kt-buttons="true">
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary active px-4 me-1" id="kt_charts_widget_6_sales_btn">Sales</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4 me-1" id="kt_charts_widget_6_expenses_btn">Expenses</a>
+												</div>
+												<!--end::Toolbar-->
+											</div>
+											<!--end::Header-->
+											<!--begin::Body-->
+											<div class="card-body">
+												<!--begin::Chart-->
+												<div id="kt_charts_widget_6_chart" style="height: 350px"></div>
+												<!--end::Chart-->
+											</div>
+											<!--end::Body-->
+										</div>
+										<!--end::Charts Widget 5-->
+									</div>
+								</div>
+								<!--end::Row-->
+								<!--begin::Row-->
+								<div class="row g-5 g-xl-8">
+									<div class="col-xl-6">
+										<!--begin::Charts Widget 7-->
+										<div class="card card-xl-stretch mb-xl-8">
+											<!--begin::Header-->
+											<div class="card-header border-0 pt-5">
+												<h3 class="card-title align-items-start flex-column">
+													<span class="card-label fw-bolder fs-3 mb-1">Recent Users</span>
+													<span class="text-muted fw-bold fs-7">More than 500 new users</span>
+												</h3>
+												<!--begin::Toolbar-->
+												<div class="card-toolbar" data-kt-buttons="true">
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary active px-4 me-1" id="kt_charts_widget_7_year_btn">Year</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4 me-1" id="kt_charts_widget_7_month_btn">Month</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4" id="kt_charts_widget_7_week_btn">Week</a>
+												</div>
+												<!--end::Toolbar-->
+											</div>
+											<!--end::Header-->
+											<!--begin::Body-->
+											<div class="card-body">
+												<!--begin::Chart-->
+												<div id="kt_charts_widget_7_chart" style="height: 350px" class="card-rounded-bottom"></div>
+												<!--end::Chart-->
+											</div>
+											<!--end::Body-->
+										</div>
+										<!--end::Charts Widget 7-->
+									</div>
+									<div class="col-xl-6">
+										<!--begin::Charts Widget 8-->
+										<div class="card card-xl-stretch mb-5 mb-xl-8">
+											<!--begin::Header-->
+											<div class="card-header border-0 pt-5">
+												<h3 class="card-title align-items-start flex-column">
+													<span class="card-label fw-bolder fs-3 mb-1">Recent Orders</span>
+													<span class="text-muted fw-bold fs-7">More than 500 new orders</span>
+												</h3>
+												<!--begin::Toolbar-->
+												<div class="card-toolbar" data-kt-buttons="true">
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary active px-4 me-1" id="kt_charts_widget_8_year_btn">Year</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4 me-1" id="kt_charts_widget_8_month_btn">Month</a>
+													<a class="btn btn-sm btn-color-muted btn-active btn-active-primary px-4" id="kt_charts_widget_8_week_btn">Week</a>
+												</div>
+												<!--end::Toolbar-->
+											</div>
+											<!--end::Header-->
+											<!--begin::Body-->
+											<div class="card-body">
+												<!--begin::Chart-->
+												<div id="kt_charts_widget_8_chart" style="height: 350px" class="card-rounded-bottom"></div>
+												<!--end::Chart-->
+											</div>
+											<!--end::Body-->
+										</div>
+										<!--end::Charts Widget 8-->
+									</div>
+								</div>
+
                 <!--begin::Row-->
                 <div class="row gy-5 g-xl-8">
                     <!--begin::Col-->
